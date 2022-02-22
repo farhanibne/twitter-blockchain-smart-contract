4c9534926bb21fe1a2de67ad0c1a7ec23348d699
farhan ibne saif <84982846+farhanibne@users.noreply.github.com>
2022-02-22 11:00:56 +0600

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,9 +4,9 @@ module.exports = {
   solidity: '0.8.2',
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/b-KokcQfcWU681FYTxvnylnb3Ro_tjqu',
+      url: '',  //alchemy api url goes there
       accounts: [
-        '6ebb25a38f2ef8839c78669edadf1e849497ffc2758ef9f106e9bdd834b2dfa6',
+        '',   //wallet secret phrase goes there
       ],
     },
   },
